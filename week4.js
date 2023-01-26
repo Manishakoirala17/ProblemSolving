afd0f39b5f2c10255dfde0cb5d24182f781c5fa6
Kishore-Dreamtusk <vignesh@dreamtusk.com>
2023-01-26 18:31:30 +0530

    Update week4.js

--- week4.js
+++ week4.js
@@ -1,10 +1,25 @@
 
 function sumOfBiggestNumber(number1, number2) {
-    let length = number1.length;
+    let num1, num2;
+    if (number1.length > number2.length) {
+        num1 = number1;
+        num2 = number2;
+    }
+    else {
+        num1 = number2;
+        num2 = number1
+    }
+
+    let length = num1.length;
+    let diff = num1.length - num2.length;
+
     let sum = [];
     let carry = 0;
+
     for (let index = length - 1; index >= 0; index--) {
-        let add = parseInt(number1[index]) + parseInt(number2[index]) + carry;
+        let index2 = index - diff;
+        let add = parseInt(num1[index]) + parseInt(index2 >= 0 ? num2[index2] : 0) + carry;
+
         carry = Math.floor(add / 10);
         let remain = add % 10;
         sum[index] = index == 0 ? (remain + carry * 10).toString() : sum[index] = remain.toString();
@@ -12,8 +27,8 @@ function sumOfBiggestNumber(number1, number2) {
 
     return sum.join("");
 }
-let num1 = "888888888";
-let num2 = "666666666";
+let num1 = "8888";
+let num2 = "666666";
 
 let additon = sumOfBiggestNumber(num1, num2)
 
